f185b33eefe9337f53d541b068818ab2c163fa76
Yuanqing Zhu <yuanqingzhu@microsoft.com>
2022-04-02 18:58:10 +0800

    Change color by using rgb(r,g,b)

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
 // This is a background script. 
 // Essentially this is a service worker.
+// Colors: http://zhongguose.com
 
-function reddenPage() {
-  document.body.style.backgroundColor = 'red';
+function setBackgroundColor(r, g, b) {
+  let colorStyle = "rgb(" + r + "," + g + "," + b + ")";
+  console.log("Set background color: ", colorStyle);
+  document.body.style.backgroundColor = colorStyle;
 }
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: reddenPage
+    function: setBackgroundColor,
+    args: [237,85,106] // 苋菜红
   });
 });
 
